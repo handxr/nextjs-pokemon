f8ac36e1d78d46f9450498bc31033b1f3a913c23
Han <88827764+handxr@users.noreply.github.com>
2022-12-30 11:40:56 +0100

    optimize image

--- features/pokemon/components/pokemon-card.tsx
+++ features/pokemon/components/pokemon-card.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/navigation";
 import { Grid, Card, Row, Text } from "@nextui-org/react";
 import { PokemonListSingle } from "../domain/types/pokemon-list-single";
+import Image from "next/image";
 
 type PokemonCardProps = {
   pokemon: PokemonListSingle;
@@ -21,7 +22,17 @@ export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
             padding: 1,
           }}
         >
-          <Card.Image src={pokemon.image} width="100%" height={140} />
+          <Image
+            src={pokemon.image}
+            width={100}
+            height={140}
+            style={{
+              width: "100%",
+            }}
+            alt={pokemon.name}
+            placeholder="blur"
+            blurDataURL={pokemon.image}
+          />
           <Card.Footer>
             <Row justify="space-between">
               <Text transform="capitalize">{pokemon.name}</Text>
