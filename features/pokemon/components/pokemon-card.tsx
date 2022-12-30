cf80c885da24c2e13ea36fa6e1757e5892201271
Han <88827764+handxr@users.noreply.github.com>
2022-12-30 11:49:47 +0100

    adding topbar

--- features/pokemon/components/pokemon-card.tsx
+++ features/pokemon/components/pokemon-card.tsx
@@ -28,6 +28,7 @@ export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
             height={140}
             style={{
               width: "100%",
+              height: "100%",
             }}
             alt={pokemon.name}
             placeholder="blur"
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "styles/globals.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 import { darkTheme } from "themes";
 
 export default function App({ Component, pageProps }: AppProps) {
